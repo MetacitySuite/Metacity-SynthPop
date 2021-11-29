880cbe5566de767dc5516602d9c06ad892b149ee
AnnaMoudra <anna.moudra@gmail.com>
2021-11-29 17:59:58 +0100

    Extracted trip distances.

--- synthesis/locations/matched_work.py
+++ synthesis/locations/matched_work.py
@@ -36,16 +36,24 @@ def configure(context):
 
 """
 
-
 def extract_commute_distances(df_people, df_travelers, df_trips):
     distances = []
-
-    #print(df_trips.beeline)
-
     people_travelers = df_people.merge(df_travelers, left_on="hdm_source_id", right_on="traveler_id")
-    #print(people_travelers.beeline.
+    df_trips = df_trips[df_trips.traveler_id.isin(people_travelers.hdm_source_id.unique())]
+    work_trips = df_trips[df_trips.destination_purpose == "work"]
+
+    #fill in beeline - use origin, destination code (27perc missing)
+    beeline_median = work_trips.groupby(['origin_code', 'destination_code'])['beeline'].transform('median')
+    work_trips.loc[:, 'beeline'] = work_trips['beeline'].fillna(value=beeline_median)
+    #fill rest with median
+    beeline_median = work_trips['beeline'].median()
+    work_trips.loc[:, 'beeline'] = work_trips['beeline'].fillna(value=beeline_median)
+    
+
+    people_travelers = people_travelers.merge(work_trips, left_on="traveler_id", right_on="traveler_id")
 
     #df_people["commute_distance"] = distances
+    df_people = people_travelers.drop(['trip_order', 'origin_code','destination_code', 'departure_h', 'departure_m', 'arrival_h', 'arrival_m', 'traveler_id'], axis=1)
 
     return df_people
 
@@ -71,11 +79,19 @@ def execute(context):
 
 
     #Assigning primary location (work): Step 3
+
+    
     df_u = df_matched.loc[df_matched.person_id.isin(df_employed_ids)]
+    # extract distances
     df_u = extract_commute_distances(df_u, df_travelers, df_trips)
     print(df_u.head())
+    print(df_u.beeline.value_counts())
 
-    #df_home_zones = df_matched.iloc[df_employed_ids].groupby("zone_id")
+
+    # assign trips in C_kk
+    df_home_zones = df_u.iloc[df_employed_ids].groupby("zone_id")
+
+    #
     #print(len(df_home_zones))
 
     pass
